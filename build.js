17d82bac7df83130aaf4e93fbef72679f269e45b
Marijn Haverbeke <marijn@haverbeke.nl>
2020-04-01 11:57:24 +0200

    Make the package work as a dual-module-style node package

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
-const fs = require("fs"), tariff = require("tariff")
+import * as fs from "fs"
+import tariff from "tariff"
 
-fs.writeFileSync("ist.cjs.js",
+fs.writeFileSync("ist.cjs",
                  "// (This file is generated from ist.js. Don't edit it directly.)\n\n" +
                  tariff(fs.readFileSync("ist.js", "utf8")))
